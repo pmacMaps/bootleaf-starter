439d56add7ec57a4dcc3e99e35baa0e6bab29d54
Patrick McKinney <pnmcartography@gmail.com>
2016-07-28 14:40:25 -0400

    call map app modules

--- js/app.js
+++ js/app.js
@@ -14,11 +14,27 @@ var returnMuni = [];
 var muniBounds;
 // map and controls
 var map;
-var homeCoords = [];
-var initZoom = ;
+var homeCoords = [40.263044, -76.896423]; // can use instead of function
+var initZoom = 15;
 var container;
-var zoomHome;
+var zoomHomeControl;
 // layer control
 var basemapGroup;
 var overlayGroup;
 var layerControl;
+// Layers - not sure about this
+// ESRI service
+// GeoJSON
+// simple vector format
+
+// Create map
+mapInit();
+
+// Add layers
+layersInit();
+
+// GeoLocate module
+geoLocater();
+
+// Address Locator
+addressLocator();
